86e0bb13c9de8d03838513fd841502112179ceaf
qqHomie <n_zhaksylykov@kbtu.kz>
2021-03-12 21:03:55 +0600

    ReGex

--- tsis4/1/a.py
+++ tsis4/1/a.py
@@ -1,28 +1,28 @@
 import re
 
-# with open('./data.txt', 'r', encoding = "utf-8") as f:
-#     data = f.read()
+with open('text.txt', 'r', encoding = 'utf-8') as f:
+    text = f.read()
 
-f = open('./data.txt', 'r', encoding = "utf-8")
-data = f.read()
+name_company = re.search(r'Филиал\sТОО\s\w+\s\w+', text)
+BIN = re.search(r'\d{12}?', text)
+items = re.findall(r'\d+\.\n(.*)', text)
+cnt = re.findall(r'(\d),\d{3}', text)
+price = re.findall(r'x\s([\d\s]+,\d+)', text)
+TotalPrice = re.findall(r'x\s[\d\s]+,\d+\n(.*)', text)
+date = re.search(r'(\d{2}\.\d{2}\.\d{4}\s\d{2}:\d{2}:\d{2})', text)
+address = re.search(r'г\.(.*)', text)
 
-name = re.search(r'Филиал\sТОО\s\w+\s\w+', data)
-bin1 = re.search(r'\d{12}', data)
-items = re.findall(r'\d+\.\n(.*)', data)
-cnt = re.findall(r'(\d),\d{3}', data)
-price = re.findall(r'x\s([\d\s]+,\d+)', data)
-total = re.findall(r'Стоимость\n([\d\s]+,\d+)', data)
-address = re.search(r'г\.\s[\w\-]+\,\w+\,\s[\w+\s]+\,\d+\,\s\w+\-\d', data)
-date = re.search(r'\d{2}\.\d{2}\.\d{4}\s\d{2}\:\d{2}\:\d{2}', data)
 
-if name:
-    print("1. Name of the company: " + name.group())
-print("2. BIN number: " + bin1.group())
+print('1. Name of the company: ' + name_company.group())
+print('2. BIN number: ' + BIN.group())
 
+print('3. For each item:')
 for i in range(len(items)):
-    print("1. Title: " + items[i])
-    print("2. Count: " + cnt[i])
-    print("3. Unit price: " + price[i])
-    print("4. Total price: " + total[i])
-print("4. Date: " + date.group())
-print("5. Address: " + address.group())
+    print('\t' + str(i+1) + ')' + '-'*20)
+    print('\t   1. Title --> ' + items[i])
+    print('\t   2. Cout --> (' + cnt[i] + ')')
+    print('\t   3. Unit Price --> (' + price[i] + ')')
+    print('\t   4. Total Price --> (' + TotalPrice[i] + ')')
+
+print('4. Date --> ' + date.group())
+print('5. Address --> ' + address.group())
